4a957c558b45684e8eb0f121629b8731234be503
lucca816-ops <lucca816@gmail.com>
2026-06-09 20:28:25 +0900

    Update search-law.js

--- netlify/functions/search-law.js
+++ netlify/functions/search-law.js
@@ -1,4 +1,4 @@
-const LAW_BASE_URL = "https://www.law.go.kr/DRF/lawSearch.do";
+const LAW_BASE_URL = "http://www.law.go.kr/DRF/lawSearch.do";
 
 function jsonResponse(statusCode, data) {
   return {
